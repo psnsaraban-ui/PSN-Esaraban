992823c4263816e22d341d57f584a6e1ae180c2c
PSN Saraban <psnsaraban@gmail.com>
2026-09-24 17:36:45 +0700

    Add GitHub Pages entry for Apps Script app

--- Gs/Code.js
+++ Gs/Code.js
@@ -1,5 +1,5 @@
 function doGet(){
-  return HtmlService.createTemplateFromFile('index').evaluate()
+  return HtmlService.createTemplateFromFile('AppsScript').evaluate()
   .setTitle(nameSystem)
   .setFaviconUrl(logoUrl)
   .addMetaTag('viewport', 'width=device-width, initial-scale=1')
